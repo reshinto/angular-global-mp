aa103fde2acf29c8b4908bd6bc48755a21a6f5bc
reshinto <terencekong2002@gmail.com>
2022-09-09 13:16:13 +0800

    home task 3: add unit tests coverage to 100%

--- src/app/components/course-item/course-item.component.spec.ts
+++ src/app/components/course-item/course-item.component.spec.ts
@@ -2,6 +2,14 @@ import { ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { CourseItemComponent } from "./course-item.component";
 
+const initialProps = {
+  id: 1,
+  title: "test",
+  creationDate: "12082022",
+  duration: "60",
+  description: "abc",
+};
+
 describe("CourseItemComponent", () => {
   let component: CourseItemComponent;
   let fixture: ComponentFixture<CourseItemComponent>;
@@ -13,10 +21,42 @@ describe("CourseItemComponent", () => {
 
     fixture = TestBed.createComponent(CourseItemComponent);
     component = fixture.componentInstance;
+    component.course = initialProps;
     fixture.detectChanges();
   });
 
-  xit("should create", () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("should contain default course content in html", () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(
+      compiled.querySelector(".courseitem-top_title")?.textContent,
+    ).toContain("test");
+  });
+
+  it("should emit onDelete", () => {
+    spyOn(component.onDeleteCourse, "emit");
+
+    const compiled = fixture.nativeElement as HTMLElement;
+    const button = compiled.querySelector("[text=delete]");
+    button?.dispatchEvent(new Event("click"));
+
+    fixture.detectChanges();
+
+    expect(component.onDeleteCourse.emit).toHaveBeenCalledWith(initialProps);
+  });
+
+  it("should emit onEdit", () => {
+    spyOn(component.onEditCourse, "emit");
+
+    const compiled = fixture.nativeElement as HTMLElement;
+    const button = compiled.querySelector("[text=edit]");
+    button?.dispatchEvent(new Event("click"));
+
+    fixture.detectChanges();
+
+    expect(component.onEditCourse.emit).toHaveBeenCalledWith(initialProps);
   });
 });
--- src/app/components/course-list/course-list.component.spec.ts
+++ src/app/components/course-list/course-list.component.spec.ts
@@ -19,4 +19,38 @@ describe("CourseListComponent", () => {
   it("should create", () => {
     expect(component).toBeTruthy();
   });
+
+  it("should increment numOfDisplay", () => {
+    expect(component.numOfDisplay).toBe(3);
+    component.loadMoreCourses();
+    expect(component.numOfDisplay).toBe(6);
+  });
+
+  it("should test for deleteCourse console output", () => {
+    const courseProps = {
+      id: 1,
+      title: "test",
+      creationDate: "12082022",
+      duration: "60",
+      description: "abc",
+    };
+
+    console.log = jasmine.createSpy("log");
+    component.deleteCourse(courseProps);
+    expect(console.log).toHaveBeenCalled();
+  });
+
+  it("should test for editCourse console output", () => {
+    const courseProps = {
+      id: 1,
+      title: "test",
+      creationDate: "12082022",
+      duration: "60",
+      description: "abc",
+    };
+
+    console.log = jasmine.createSpy("log");
+    component.editCourse(courseProps);
+    expect(console.log).toHaveBeenCalled();
+  });
 });
