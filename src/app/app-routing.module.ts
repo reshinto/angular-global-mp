fec31a5acfe3d639fb0c4fa94c41fda920c90abe
reshinto <terencekong2002@gmail.com>
2022-09-15 04:46:21 +0800

    home task 7: add auth guard feature

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { CoursesComponent } from "./pages/courses/courses.component";
 import { LoginComponent } from "./pages/login/login.component";
 import { CourseComponent } from "./pages/course/course.component";
 import { NotFoundComponent } from "./pages/not-found/not-found.component";
+import { AuthGuard } from "./guards/auth.guard";
 
 const routes: Routes = [
   { path: "", redirectTo: "courses", pathMatch: "full" },
   { path: "login", component: LoginComponent },
   {
     path: "courses",
+    canActivate: [AuthGuard],
     children: [
       {
         path: "",
@@ -32,5 +34,6 @@ const routes: Routes = [
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
+  providers: [AuthGuard],
 })
 export class AppRoutingModule {}
